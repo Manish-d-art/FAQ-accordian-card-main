42d3a238f391cd5029c186128be35a677e5cc41d
manish mandal <76776194mm@gmail.com>
2022-09-05 17:57:00 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@
 const section2=document.querySelector('.card__section-2');
 
 section2.addEventListener('click',function(e){
-        console.log(2);
         const click=e.target.closest('.card__Question-block');
+        if(!click){
+            return;
+        }
+        click.querySelector('.Question').classList.toggle('qstn-style');
         const a=click.dataset.a;
         document.querySelector(`.Answer-${a}`).classList.toggle('show');
 })
